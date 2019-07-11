f337276c4a5454c7f7258b52c42eeff8a668a67a
Leonardo Ortiz <leonardo.ortiz@molekule.com>
2019-07-11 17:04:52 -0600

    Fixed problem with layout duplication

--- app/src/main/java/com/leohackerman/android/pokeapp/HomeFragment.kt
+++ app/src/main/java/com/leohackerman/android/pokeapp/HomeFragment.kt
@@ -1,31 +1,34 @@
 package com.leohackerman.android.pokeapp
 
 
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.leohackerman.android.pokeapp.databinding.FragmentHomeBinding
 import com.leohackerman.android.pokeapp.viewmodel.PokeApiViewModel
 
 
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val pokeApiViewModel:PokeApiViewModel = PokeApiViewModel()
-        pokeApiViewModel.searchPokemon("6")
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: PokeApiViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,R.layout.fragment_home,container,false)
+        val view = binding.root
+        return view
+
     }
 
-    override fun onCreateView(
 
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+
 
 
 }
