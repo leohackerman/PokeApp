ebd4a015b2bfafa275a6afc094b61265c26c386e
Leonardo Ortiz <leonardo.ortiz@molekule.com>
2019-07-15 18:41:09 -0600

    Moved Glide to a general function

--- app/src/main/java/com/leohackerman/android/pokeapp/HomeFragment.kt
+++ app/src/main/java/com/leohackerman/android/pokeapp/HomeFragment.kt
@@ -10,8 +10,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.Toast
-import com.bumptech.glide.Glide
 import com.leohackerman.android.pokeapp.databinding.FragmentHomeBinding
 import com.leohackerman.android.pokeapp.utils.UIUtils
 import com.leohackerman.android.pokeapp.viewmodel.PokeApiViewModel
@@ -53,7 +51,7 @@ class HomeFragment : Fragment() {
 
     private fun observeData(){
         viewModel.pokemon.observe(this, Observer {
-            Glide.with(this).load(viewModel.getAvatarFrontUrl()).into(default_avatar)
+            UIUtils.loadImageFromUrl(this,viewModel.getAvatarFrontUrl(),default_avatar)
         })
     }
 
--- app/src/main/java/com/leohackerman/android/pokeapp/utils/UIUtils.kt
+++ app/src/main/java/com/leohackerman/android/pokeapp/utils/UIUtils.kt
@@ -2,8 +2,13 @@ package com.leohackerman.android.pokeapp.utils
 
 import android.app.Activity
 import android.content.Context
+import android.support.v4.app.Fragment
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import io.reactivex.annotations.NonNull
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class UIUtils {
     companion object{
@@ -13,6 +18,13 @@ class UIUtils {
                 val inputManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 inputManager.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
             }
+        }
+
+        fun loadImageFromUrl(@NonNull fragment: Fragment, url:String?, imageView: ImageView){
+            Glide.with(fragment).
+                load(url).
+                apply(RequestOptions().fitCenter()).
+                into(imageView)
         }
     }
 }
