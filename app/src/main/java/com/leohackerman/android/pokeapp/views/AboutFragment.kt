7699b8b81fb016a02f72f0ff51de62712217e6b5
leohackerman <ortiz.morales.leonardo@gmail.com>
2019-07-21 23:45:26 -0600

    Added hyperlinks to about view

--- app/src/main/java/com/leohackerman/android/pokeapp/views/AboutFragment.kt
+++ app/src/main/java/com/leohackerman/android/pokeapp/views/AboutFragment.kt
@@ -1,10 +1,15 @@
 package com.leohackerman.android.pokeapp.views
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.*
+import android.widget.TextView
 import com.leohackerman.android.pokeapp.R
+import kotlinx.android.synthetic.main.fragment_about.*
 
 
 class AboutFragment : Fragment() {
@@ -16,6 +21,26 @@ class AboutFragment : Fragment() {
         setHasOptionsMenu(true)
 
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        about_author.setOnClickListener{
+            val myTextView = it as TextView
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://github.com/leohackerman")
+            startActivity(openURL)
+        }
+
+        about_message.setOnClickListener {
+            val myTextView = it as TextView
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://pokeapi.co")
+            startActivity(openURL)
+        }
+
+
     }
 
 
